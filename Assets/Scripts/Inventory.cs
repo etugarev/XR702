9cc3aacab633b6904e2b15c58855348815235338
Evgeny Tugarev <etugarev@gmail.com>
2016-03-12 15:55:00 +0100

    Added Blend Tree to Locomotion

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+	#region Public Fields & Properties
+	public GameObject[] items; 
+
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
