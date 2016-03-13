6338b886def69e436d87237db9a316bb4a5f97d3
Evgeny Tugarev <etugarev@gmail.com>
2016-03-13 01:07:30 +0100

    Added weapon spawning from inventory

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -7,7 +7,10 @@ public class Inventory : MonoBehaviour {
 	#region Public Fields & Properties
 	public GameObject[] items; 
 
-	public Transform inHands;
+	public Transform leftHandAttachPoint;
+	public Transform rightHandAttachPoint;
+
+//	public Transform inHands;
 
 	#endregion
 
@@ -30,10 +33,31 @@ public class Inventory : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (_playerController.isArmed && inHands.childCount == 0) {
-			GameObject go = Instantiate(items[0], inHands.position, Quaternion.identity) as GameObject;
-			go.transform.parent = inHands;
+
+		Vector3 hands = leftHandAttachPoint.position - rightHandAttachPoint.position;
+		// Add gun 
+//		if (_playerController.isArmed && inHands.childCount == 0) {
+//			GameObject go = Instantiate(items[0], inHands.position, inHands.rotation) as GameObject;
+//			go.transform.parent = inHands;
+//		}
+//		// Remove gun when unarmed
+//		if (!_playerController.isArmed && inHands.childCount == 1) {
+//			GameObject go = inHands.GetChild (0).gameObject;
+//			Destroy (go);
+//		}
+
+
+		 // Add gun 
+		if (_playerController.isArmed && rightHandAttachPoint.childCount == 0) {
+			GameObject go = Instantiate(items[0], rightHandAttachPoint.position, rightHandAttachPoint.rotation) as GameObject;
+			go.transform.parent = rightHandAttachPoint;
 		}
+		// Remove gun when unarmed
+		if (!_playerController.isArmed && rightHandAttachPoint.childCount == 1) {
+			GameObject go = rightHandAttachPoint.GetChild (0).gameObject;
+			Destroy (go);
+		}
+
 	
 	}
 	#endregion
