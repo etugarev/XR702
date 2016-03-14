545305ee08de75cfaa3e733ee88d0828ce738d92
Evgeny Tugarev <etugarev@gmail.com>
2016-03-14 23:30:49 +0100

    Started multiplayer implementation

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class NetworkManager : MonoBehaviour {
 
+	public Transform spawnSpot;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -11,6 +13,7 @@ public class NetworkManager : MonoBehaviour {
 
 	void Connect() 
 	{
+		PhotonNetwork.offlineMode = true;
 		PhotonNetwork.ConnectUsingSettings ("XR702 1.0.0 Beta");
 	}
 
@@ -20,6 +23,39 @@ public class NetworkManager : MonoBehaviour {
 		//GUILayout.Label (status);
 		GUI.Label(new Rect(10, 10, 200, 50), status);
 
-		Debug.Log (status);
+//		Debug.Log (status);
+	}
+
+	void OnJoinedLobby()
+	{
+		Debug.Log ("OnJoinedLobby");
+		//PhotonNetwork.JoinRandomRoom ();
+		PhotonNetwork.JoinOrCreateRoom("default", new RoomOptions(), null);
+	}
+
+	void OnPhotonRandomJoinFailed()
+	{
+		Debug.Log ("OnPhotonRandomJoinFailed");
+//		PhotonNetwork.JoinRoom (null);
+	}
+
+	void OnJoinedRoom()
+	{
+		Debug.Log ("OnJoinedRoom");
+		SpawnMyPlayer ();
+	}
+
+	void SpawnMyPlayer()
+	{
+		GameObject player =  PhotonNetwork.Instantiate ("Remy", spawnSpot.position, spawnSpot.rotation, 0);
+		player.GetComponent<PlayerController> ().enabled = true;
+		player.GetComponent<HeadLookController> ().enabled = true;
+		player.GetComponent<Inventory> ().enabled = true;
+		player.GetComponent<CharacterMotor> ().enabled = true;
+		player.GetComponent<PlayerAnimator> ().enabled = true;
+
+
+		player.GetComponentInChildren<Camera> ().enabled = true;
+
 	}
 }
